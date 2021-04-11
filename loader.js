41b323f2ec4415fe9ca22f383bd0dc7a3185b9d7
mmsorokin <50393762+mmsorokin@users.noreply.github.com>
2021-04-11 11:45:20 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -364,13 +364,7 @@ script.onload = function(){
   <span class="second"></span> \
   <span class="third"></span> \
 </label> \
-<ul class="hidden-menu"> \
-    <a href="#config" data-transition="pop" class="ui-btn ui-shadow ui-btn ui-btn-a">System Setup</a> \
-    <a href="#modules" data-transition="pop" class="ui-btn ui-shadow ui-btn ui-btn-a">Module Setup</a>  \
-    <a id="AboveAvgBalance" class="ui-btn ui-shadow ui-btn ui-btn-a">Average Balance</a> \
-    <a id="CancelAvgBalance" class="ui-btn ui-shadow ui-btn ui-btn-a">Cancel Average Balance</a> \
-    <a id="ResetESP" class="ui-btn ui-shadow ui-btn ui-btn-a">System Reset</a> \
-</ul> \
+
 	<div data-role="page" id="config" data-dom-cache="true"> \
 	<div data-role="header"><h1>Configuration</h1></div> \
 	<div role="main" data-role="ui-content"> \
